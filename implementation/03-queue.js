1f79d363909b9159ea7b8e80587dcff636890baf
Jillian Armonia <jillian.marice@gmail.com>
2024-07-18 22:10:31 +0900

    passed queue tests

--- implementation/03-queue.js
+++ implementation/03-queue.js
@@ -5,18 +5,40 @@ class Queue {
     constructor() {
         this.head = null;
         this.tail = null;
+        this.length = 0;
     }
 
     enqueue(val) {
-        // Add node to end of queue (linked list)
+        let newNode = new SinglyLinkedNode(val);
 
-        // Write your hypothesis on the time complexity of this method here
+        if (!this.head && !this.tail){
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+
+        this.length++;
+        return this.length;
+        // Write your hypothesis on the time complexity of this method here O(1)
     }
 
     dequeue() {
-        // Remove node from front of queue (linked list)
-        
-        // Write your hypothesis on the time complexity of this method here
+        let removed = this.head;
+
+        if(this.length > 1){
+            this.head = removed.next;
+            this.length--;
+            return removed.value;
+        } else if (this.length === 1){
+            this.head = null;
+            this.tail = null;
+            this.length--;
+            return removed.value;
+        } else return null;
+
+        // Write your hypothesis on the time complexity of this method here O(1)
     }
 
 }
